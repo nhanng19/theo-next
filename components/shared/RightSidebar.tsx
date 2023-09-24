4f0404318ef3b28110358c249ce74f77af858da8
Nhan <nhanng19@gmail.com>
2023-09-23 20:19:44 -0700

    components/shared/RightSidebar.tsx committed

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,14 @@
+const RightSidebar = () => {
+    return (
+        <section className="customer-scrollbar rightsidebar">
+            <div className="flex flex-col flex-1 justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+            </div>
+            <div className="flex flex-col flex-1 justify-start">
+                <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+            </div>
+        </section>
+    )
+}
+
+export default RightSidebar;
