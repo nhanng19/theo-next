5c8a3ffd28045f0bc393432ff3a022091b020fac
Nhan <nhanng19@gmail.com>
2023-09-24 02:50:19 -0700

    change components/shared/Bottombar.tsx

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { sidebarLinks } from "@/contants";
+import { sidebarLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import {usePathname , useRouter} from "next/navigation";
