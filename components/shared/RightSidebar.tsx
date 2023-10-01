599a50a7afb5a1d5fad55a742907e5cc9d776047
Nhan <nhanng19@gmail.com>
2023-09-30 23:05:53 -0700

    modified RightSidebar.tsx

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,10 +2,10 @@ const RightSidebar = () => {
     return (
         <section className="customer-scrollbar rightsidebar">
             <div className="flex flex-col flex-1 justify-start">
-                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
+                <h3 className="text-heading4-medium text-light-1">Suggested Verses</h3>
             </div>
             <div className="flex flex-col flex-1 justify-start">
-                <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+                <h3 className="text-heading4-medium text-light-1">Suggested Communities</h3>
             </div>
         </section>
     )
